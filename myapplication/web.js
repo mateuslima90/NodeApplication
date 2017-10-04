7fb36002d1364f7fe4eb5d5dfb4b6fff18a73501
lab <makingthehand@ubuntulab.com>
2017-10-03 22:09:43 -0300

    Comitando a aplicacao

---
+++ myapplication/web.js
@@ -0,0 +1,32 @@
+
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  //Fork Workers
+   for (let i = 0; i <numCPUs; i++) {
+	cluster.fork();
+   }
+
+   cluster.on('exit', (worker,code, signal) => {
+	console.log(`worker ${worker.process.pid} died`);
+	});
+}else {
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res){
+res.send('Hello World!');
+//res.send(`${worker.process.pid}`);
+});
+
+app.listen(3000,function() {
+console.log('Example app listening on port 3000!')
+});
+
+  console.log(`Worker ${process.pid} started`);
+}
