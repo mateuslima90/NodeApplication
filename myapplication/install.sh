a88caf2743111bb069c92d578b64aa738e1ed9fc
lab <makingthehand@ubuntulab.com>
2017-10-06 03:56:04 -0300

    envio script para instalacao

---
+++ myapplication/install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y curl
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
+source ~/.profile
+nvm ls-remote
+nvm install v6.11.4
+npm install express
+node /home/makinghand/myapplication/web.js
