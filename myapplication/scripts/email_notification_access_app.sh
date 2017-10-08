7f39cab272407d3b8d9af2f680471b03af25e645
lab <makingthehand@ubuntulab.com>
2017-10-08 03:40:19 -0300

    ajustado notificacao automatica de eamil, retirada do usuario para envio de email muttrc e criacao do arquivo notification.txt

--- myapplication/scripts/email_notification_access_app.sh
+++ myapplication/scripts/email_notification_access_app.sh
@@ -3,7 +3,7 @@
 file="$1"
 receiver="$2"
 
-	awk -F " " '{print $7,$9}' "$file" | sort | uniq --count | tr -d '[[:blank:]]' > /home/makingthehand/myapplication/scripts/notification.txt;
+	awk -F " " '{print $7,$9}' "$file" | sort | uniq --count | tr -d '[[:blank:]]' > notification.txt;
 
-	echo "Notification access log of Node Application" | mutt -s 'Notification' -a /home/makingthehand/myapplication/scripts/notification.txt -- "$receiver";
+	echo "Notification access log of Node Application" | mutt -s 'Notification' -a notification.txt -- "$receiver";
 
